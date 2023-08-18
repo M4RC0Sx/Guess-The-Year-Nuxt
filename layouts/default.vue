f4eac2179ea885c534334a1a87d8e1c320a63ea9
M4RC0Sx <marcos3as3@gmail.com>
2023-08-18 18:41:08 +0200

    feat(layout): add default layout with simple navbar

---
+++ layouts/default.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>
+    <ul>
+      <li><NuxtLink to="/">Index</NuxtLink></li>
+      <li><NuxtLink to="/game">Game</NuxtLink></li>
+    </ul>
+    <div>
+      <slot />
+    </div>
+  </div>
+</template>
