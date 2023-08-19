8844ca7f1e16b8f10d7fc93ee65298abe75ca4e9
M4RC0Sx <marcos3as3@gmail.com>
2023-08-19 11:10:33 +0200

    feat: tailwindcss set up

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,4 +4,11 @@ export default defineNuxtConfig({
   app: {
     pageTransition: { name: "page", mode: "out-in" },
   },
+  css: ["~/assets/css/main.css"],
+  postcss: {
+    plugins: {
+      tailwindcss: {},
+      autoprefixer: {},
+    },
+  },
 });
