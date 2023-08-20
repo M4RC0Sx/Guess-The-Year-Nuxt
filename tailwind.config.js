001c0e7759a609ba6ae43deb73e83b3010345822
M4RC0Sx <marcos3as3@gmail.com>
2023-08-20 11:17:13 +0200

    feat(config): configure tailwind and nuxt with svgo and formkit

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,10 @@
 /** @type {import('tailwindcss').Config} */
+
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const FormKitVariants = require("@formkit/themes/tailwindcss");
+
 module.exports = {
+  darkMode: "class",
   content: [
     "./components/**/*.{js,vue,ts}",
     "./layouts/**/*.vue",
@@ -7,9 +12,10 @@ module.exports = {
     "./plugins/**/*.{js,ts}",
     "./nuxt.config.{js,ts}",
     "./app.vue",
+    "./themes/tailwind-formkit-theme.js",
   ],
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [FormKitVariants],
 };
