672185153a5196067267e41a997332806b1f97e7
M4RC0Sx <marcos3as3@gmail.com>
2023-08-20 11:20:47 +0200

    feat(theme): handle theme switch when app is mounted

--- app.vue
+++ app.vue
@@ -5,6 +5,28 @@
   </div>
 </template>
 
+<script setup lang="ts">
+type Theme = "light" | "dark";
+const LOCAL_STORAGE_THEME_KEY = "theme";
+
+onMounted(() => {
+  const isDarkModePreferred = window.matchMedia(
+    "(prefers-color-scheme: dark)",
+  ).matches;
+  const themeFromLocalStorage = localStorage.getItem(
+    LOCAL_STORAGE_THEME_KEY,
+  ) as Theme;
+
+  if (themeFromLocalStorage) {
+    document.documentElement.classList.add(themeFromLocalStorage);
+  } else {
+    document.documentElement.classList.remove(
+      isDarkModePreferred ? "dark" : "light",
+    );
+  }
+});
+</script>
+
 <style>
 .page-enter-active,
 .page-leave-active {
