5149ec4ff08a84f7f82352334ef85839074e896d
M4RC0Sx <marcos3as3@gmail.com>
2023-08-20 11:18:11 +0200

    feat(navbar): base navbar with light/dark theme switch

---
+++ components/NavBarComponent.vue
@@ -0,0 +1,57 @@
+<template>
+  <nav
+    class="flex space-x-4 py-4 mx-2 bg-white dark:bg-gray-900 border-b border-gray-200 dark:border-gray-600"
+  >
+    <div
+      class="flex flex-auto items-center justify-between mx-auto p-4 max-w-4xl"
+    >
+      <ul class="flex items-center list-none">
+        <a class="dark:text-gray-200" @click="darkMode = !darkMode">
+          <IconMoon
+            v-if="darkMode"
+            class="w-6 h-6"
+            :font-controlled="false"
+          ></IconMoon>
+          <IconSun v-else class="w-6 h-6" :font-controlled="false"></IconSun>
+        </a>
+      </ul>
+      <ul class="flex items-center list-none dark:text-gray-200">
+        <NuxtLink class="md:text-5xl text-xl font-bold" to="/"
+          >Guess The Year</NuxtLink
+        >
+      </ul>
+      <ul class="flex items-center list-none">
+        <a
+          class="dark:text-gray-200"
+          href="https://github.com/M4RC0Sx/Guess-The-Year-Nuxt"
+        >
+          <IconGitHub class="w-6 h-6" :font-controlled="false" />
+        </a>
+      </ul>
+    </div>
+  </nav>
+</template>
+
+<script setup lang="ts">
+import IconGitHub from "~/assets/img/icons/github.svg";
+import IconSun from "~/assets/img/icons/sun.svg";
+import IconMoon from "~/assets/img/icons/moon.svg";
+
+type Theme = "light" | "dark";
+const LOCAL_STORAGE_THEME_KEY = "theme";
+
+const darkMode = useState("theme", () => false);
+
+const setTheme = (theme: Theme) => {
+  localStorage.setItem(LOCAL_STORAGE_THEME_KEY, theme);
+  if (theme === "dark") {
+    document.documentElement.classList.add("dark");
+  } else {
+    document.documentElement.classList.remove("dark");
+  }
+};
+
+watch(darkMode, (value) => {
+  setTheme(value ? "dark" : "light");
+});
+</script>
