2b6915c047951ef1bed41ea9e891739454680253
M4RC0Sx <marcos3as3@gmail.com>
2023-08-29 20:46:06 +0200

    feat(game): post answer event handling and game status enum

--- pages/game.vue
+++ pages/game.vue
@@ -14,23 +14,46 @@
         </div>
       </div>
     </div>
-    <QuestionComponent
-      v-if="!gameFinished"
-      :question="questions[questionIndex]"
-      @answer-event="onAnswerEvent"
-    />
-    <h1 v-else class="text-4xl font-bold text-center">
-      Congratulations! You finished the game!
-    </h1>
+    <div v-if="gameStatus === GameStatus.STARTED">
+      <QuestionComponent
+        v-if="!answered"
+        :question="questions[questionIndex]"
+        @answer-event="onAnswerEvent"
+      />
+      <AnsweredQuestionComponent
+        v-else
+        :question="questions[questionIndex]"
+        :answered-year="answeredYear"
+        :lives="lives"
+        @post-answer-event="
+          questionIndex += 1;
+          answered = false;
+        "
+      />
+    </div>
+    <div v-else>
+      <h1>Game finished with {{ score }} score!</h1>
+      <button
+        class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 border border-blue-700 rounded mb-4"
+      >
+        <NuxtLink to="/">Return to home</NuxtLink>
+      </button>
+    </div>
   </div>
 </template>
 
 <script setup lang="ts">
+import { GameStatus } from "~/types/game-status.d";
+
 import questions from "~/assets/questions.json";
 
-const gameFinished = ref(false);
+const gameStatus = ref(GameStatus.STARTED);
+
 const score = ref(0);
 const lives = ref(100);
+
+const answered = ref(false);
+const answeredYear = ref(0);
 
 // Unoptimized shuffle function, DO NOT USE ON SERIOUS PROJECTS!
 questions.sort(() => Math.random() - 0.5);
@@ -38,12 +61,21 @@ questions.sort(() => Math.random() - 0.5);
 const questionIndex = ref(0);
 
 function onAnswerEvent(answer: number) {
+  answered.value = true;
+  answeredYear.value = answer;
+
   const correctAnswer = questions[questionIndex.value].year;
   if (correctAnswer === answer) {
     score.value += 1;
   } else {
     lives.value -= Math.abs(correctAnswer - answer);
   }
-  questionIndex.value += 1;
 }
+
+watch(lives, (value) => {
+  if (value <= 0) {
+    lives.value = 0;
+    gameStatus.value = GameStatus.LOST;
+  }
+});
 </script>
