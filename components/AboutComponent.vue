a7e75ee13f6032f693c766a40d0419173ba0d164
M4RC0Sx <marcos3as3@gmail.com>
2023-09-02 09:33:25 +0200

    feat(style): improve global style (#5, #4)

--- components/AboutComponent.vue
+++ components/AboutComponent.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="flex flex-col justify-center gap-6 w-full items-center">
     <div
-      class="flex flex-col items-center justify-center bg-gray-300 dark:bg-gray-600 h-16 max-w-xl px-8 rounded-md"
+      class="flex flex-col items-center justify-center bg-theme4 dark:bg-dark-theme2 h-16 max-w-xl px-8 rounded-md"
     >
       <div class="flex flex-row gap-x-3">
         <github-button
@@ -63,30 +63,32 @@
       </div>
     </div>
     <div
-      class="flex flex-col items-center justify-center bg-gray-300 dark:bg-gray-600 max-w-xl px-8 mx-4 rounded-md"
+      class="flex flex-col items-center justify-center bg-theme4 dark:bg-dark-theme2 max-w-xl px-8 mx-4 rounded-md"
     >
       <div class="flex flex-row mt-3">
-        <span class="text-2xl font-bold text-center"
+        <span class="text-xl font-bold text-center font-roboto"
           >Guess the year where each event took place!</span
         >
       </div>
       <div class="flex flex-col justify-items-start w-full mt-5 mb-4">
-        <span class="font-semibold justify-self-start"> Instructions: </span>
-        <ul class="ml-5 list-disc">
+        <span class="font-semibold justify-self-start font-roboto">
+          Instructions:
+        </span>
+        <ul class="ml-5 list-disc font-roboto">
           <li>Answers range: 1900-2023</li>
           <li>Playing time: 1-2 minutes</li>
         </ul>
       </div>
       <button
-        class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 border border-blue-700 rounded mb-4"
+        class="bg-dark-theme1 hover:bg-dark-theme2 dark:hover:bg-dark-theme3 text-white font-bold py-2 px-4 border border-dark-theme4 rounded mb-4 font-roboto"
       >
         <NuxtLink to="/game">Let's Play!</NuxtLink>
       </button>
     </div>
     <div
-      class="flex flex-col items-center justify-center bg-gray-300 dark:bg-gray-600 max-w-xl px-8 mx-4 rounded-md"
+      class="flex flex-col items-center justify-center bg-theme4 dark:bg-dark-theme2 max-w-xl px-8 mx-4 rounded-md"
     >
-      <div class="py-4">
+      <div class="py-4 font-roboto">
         <p>
           This project is based on
           <a href="https://guess-the-year.davjhan.com/"
